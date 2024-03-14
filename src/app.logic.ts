9309ced4cadfa2dd9c1e229622208e539d4f9ea4
Elvis Merino <emerinofarfan@gmail.com>
2024-03-13 23:01:52 -0500

    fin seccion 6

---
+++ src/app.logic.ts
@@ -0,0 +1,32 @@
+import fs from 'fs';
+import { yarg } from './config/plugins/yargs.plugin';
+
+const { b:base, l:limit, s:showTable } = yarg;
+
+
+
+let outputMessage = '';
+const headerMessage = `
+=================================================
+            Tabla del ${base}
+=================================================\n
+`;
+
+for (let i = 1; i <= limit; i++) {
+    outputMessage += `${base} x ${i} = ${base * i}\n`;
+}
+
+outputMessage = headerMessage + outputMessage;
+
+if ( showTable ) {
+    console.log( outputMessage );
+}
+
+//const outputPath = `outputs/folder1/folder2/folder3`;
+const outputPath = `outputs`;
+
+//const outputPath = `outputs`
+
+fs.mkdirSync( outputPath , { recursive: true } );
+fs.writeFileSync( `${ outputPath }/tabla-${ base }.txt`, outputMessage );
+console.log('Archivo credo');
